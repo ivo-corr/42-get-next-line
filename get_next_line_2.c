8ffc996379264d019f37ea326cf800c023cd4530
ivo-corr <ivoc@gmx.de>
2026-05-18 21:27:47 +0200

    Create get_next_line_2.c

---
+++ get_next_line_2.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icorrale <icorrale@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/08 09:57:32 by icorrale          #+#    #+#             */
+/*   Updated: 2026/05/15 16:28:25 by icorrale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*extract_line(char *stash)
+{
+	char	*l;
+	int		slen;
+
+	slen = ft_strchr(stash, '\0') - stash;
+	if (ft_strchr(stash, '\n'))
+	{
+		l = malloc((ft_strchr(stash, '\n') - stash) + 2 * sizeof(char));
+		if (!l)
+			return (NULL);
+		ft_strlcpy(l, stash, ft_strchr(stash, '\n') - stash + 1);
+		ft_strlcat(l, "\n", (ft_strchr(stash, '\n') - stash) + 2);
+		ft_strlcpy(stash, ft_strchr(stash, NL) + 1, slen + 2);
+		return (l);
+	}
+	else
+		return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char		buff[BUFFER_SIZE + 1];
+	static int		bread = BUFFER_SIZE;
+	static char		*sbuff = NULL;
+
+	if (bread <= 0)
+		return (NULL);
+	if ((sbuff && ft_strchr(sbuff, '\n')) || bread == 0)
+		return (extract_line(sbuff));
+	sbuff = ft_r_read_line(fd, buff, sbuff, &bread);
+	if (!sbuff || *sbuff == '\0' || bread == -1)
+	{
+		free (sbuff);
+		sbuff = NULL;
+		return (NULL);
+	}
+	return (extract_line(sbuff));
+}
