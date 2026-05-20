5c7988e5e18799a80237ada0095758e67efc382b
ivo-corr <ivoc@gmx.de>
2026-05-20 07:49:11 +0200

    Update get_next_line_2.h

--- get_next_line_2.h
+++ get_next_line_2.h
@@ -6,7 +6,7 @@
 /*   By: icorrale <icorrale@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 09:57:44 by icorrale          #+#    #+#             */
-/*   Updated: 2026/05/15 12:31:55 by icorrale         ###   ########.fr       */
+/*   Updated: 2026/05/19 10:27:10 by icorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stddef.h>
-# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
@@ -25,11 +24,10 @@
 # define NL '\n'
 
 char	*get_next_line(int fd);
-char	*ft_r_read_line(int fd, char *buff, char *sbuff, int *bread);
-char	*expand_sbuff(char *current, char *buff);
+void	ft_r_read_line(int fd, char *buff, char **sbuff, int *bread);
+void	expand_sbuff(char **current, char *buff);
 char	*ft_strchr(const char *s, int c);
-void	ft_strlcat(char *dst, const char *src, size_t size);
-void	ft_strlcpy(char *dst, const char *src, size_t size);
-char	*extract_line(char *stash);
+void	ft_cpycat(char *dst, const char *src, size_t size, int m);
+void	*free_stash(char **sbuff);
 
 #endif
