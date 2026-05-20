7224be6de315f573185561e522c965db03afb7af
ivo <icorrale@student.42heilbronn.de>
2026-05-20 16:11:47 +0200

    some progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: icorrale <icorrale@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 09:57:32 by icorrale          #+#    #+#             */
-/*   Updated: 2026/05/20 10:23:19 by icorrale         ###   ########.fr       */
+/*   Updated: 2026/05/20 16:10:29 by icorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,18 @@ static char	*extract_line(char *stash)
 	{
 		l = malloc((ft_strchr(stash, '\n') - stash) + 2 * sizeof(char));
 		if (!l)
-		{
-			return (free_stash(&stash));
-		}
+			return (NULL);
 		ft_cpycat(l, stash, ft_strchr(stash, '\n') - stash + 1, 0);
 		ft_cpycat(l, "\n", (ft_strchr(stash, '\n') - stash) + 2, 1);
-		ft_cpycat(stash, ft_strchr(stash, NL) + 1, slen + 2, 0);
+		ft_cpycat(stash, ft_strchr(stash, NL) + 1, slen + 1, 0);
 		return (l);
 	}
 	else
 	{
-		l = malloc((ft_strchr(stash, '\0') - stash) + 2 * sizeof(char));
+		l = malloc((ft_strchr(stash, '\0') - stash) + 1 * sizeof(char));
 		if (!l)
-			return (free_stash(&stash));
-		ft_cpycat(l, stash, slen + 2, 0);
+			return (NULL);
+		ft_cpycat(l, stash, slen + 1, 0);
 		*stash = '\0';
 		return (l);
 	}
@@ -43,13 +41,15 @@ static char	*extract_line(char *stash)
 
 char	*get_next_line(int fd)
 {
-	static char		buff[BUFFER_SIZE];
+	static char		buff[BUFFER_SIZE + 1];
 	static int		bread = BUFFER_SIZE;
 	static char		*sbuff = NULL;
 
 	if ((sbuff && ft_strchr(sbuff, '\n')))
 		return (extract_line(sbuff));
 	sbuff = ft_r_read_line(fd, buff, sbuff, &bread);
+	if (!sbuff)
+		return (NULL);
 	if (!sbuff || *sbuff == '\0' || bread == -1)
 	{
 		bread = BUFFER_SIZE;
